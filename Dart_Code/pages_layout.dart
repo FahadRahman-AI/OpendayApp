fe3071d48e09dff85a0197b4b72e927322138776
JoshB94 <joshbowdley@hotmail.com>
2025-05-07 21:39:22 +0100

    Update pages_layout.dart

--- Dart_Code/pages_layout.dart
+++ Dart_Code/pages_layout.dart
@@ -3,6 +3,8 @@ import 'about_us.dart';
 import 'contact_us.dart';
 import 'what_to_do.dart';
 import 'intro_screen.dart';
+import 'map.dart';
+import 'account_screen.dart';
 
 
 ///The following code outlines the basic page layout used across all pages.
@@ -19,6 +21,8 @@ class BaseScaffold extends StatelessWidget {
   final bool showMenu; // New parameter to control menu visibility
   final Widget? leading; //Leading refers to beginning of app bar, '?' signifies it can be null
   final bool automaticallyImplyLeading; //Parameter to control back button
+  final bool showAccount;
+
 
   const BaseScaffold({
     Key? key,
@@ -27,21 +31,27 @@ class BaseScaffold extends StatelessWidget {
     this.showMenu = true, // Default value: true (menu is shown by default)
     this.automaticallyImplyLeading = false,
     this.leading,
+    this.showAccount = true, //Show Account menu button on right side of the app bar
   }) : super(key: key);
 
   //Set a function to move around the app on menu button
   void _navigate(BuildContext context, String newValue) {
     if (newValue == 'About Us') {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => AboutUs()));
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => AboutUs()));
     } else if (newValue == 'Contact Us') {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => ContactUs()));
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ContactUs()));
     } else if (newValue == 'What To Do') {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => WhatToDo()));
-    } else if (newValue == 'Introduction') {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => IntroScreen()));
-    } //else if (newValue == 'QR Scan'){
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => WhatToDo()));
+    } else if (newValue == 'Getting Around') {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => GettingAround()));
+      //else if (newValue == 'QR Scan'){
       //Navigator.push(context, MaterialPageRoute(builder: (context) => QRScanPage()));
-    //}
+      //}
+    }
   }
 
   //Create the UI
@@ -51,22 +61,43 @@ class BaseScaffold extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
         backgroundColor: Colors.blueGrey,
-        automaticallyImplyLeading: automaticallyImplyLeading, //Remove back button
-        leading: showMenu // Check if menu should be displayed, show it can be null
+        automaticallyImplyLeading: automaticallyImplyLeading,
+        leading: showMenu
             ? PopupMenuButton<String>(
           icon: Icon(Icons.menu, color: Colors.black),
           onSelected: (String newValue) => _navigate(context, newValue),
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          itemBuilder: (BuildContext context) =>
+          <PopupMenuEntry<String>>[
             PopupMenuItem(value: 'About Us', child: Text('About Us')),
             PopupMenuItem(value: 'Contact Us', child: Text('Contact Us')),
             PopupMenuItem(value: 'What To Do', child: Text('What To Do')),
-            PopupMenuItem(value: 'Introduction', child: Text('Introduction')),
-            //PopupMenuItem(value: 'QR Scan', child: Text('Qr Scan')),
+            PopupMenuItem(
+                value: 'Getting Around', child: Text('Getting Around')),
           ],
         )
-            : null, // If showMenu is false, remove the leading widget, ternary operator.
+            : null,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle, color: Colors.grey),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AccountScreen()),
+              );
+            },
+          )
+        ],
       ),
-      body: body,
+      body: Column(
+        children: [
+          Expanded(child: body), // Your main page content
+          Image.asset(
+            'images/bottom_banner.png',
+            width: double.infinity,
+            fit: BoxFit.fill,
+          ),
+        ],
+      ),
     );
   }
 }
